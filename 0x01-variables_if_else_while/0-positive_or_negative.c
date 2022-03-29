cfa5882beba2648ddf88472bc10c3c5af7aeaa10
ekene <ekenesam39@gmail.com>
2022-03-29 04:57:46 -0700

    modified commit for 0 positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,19 +15,19 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if (n==0)
+	if (n == 0)
 	{
 		printf("%i is zero\n", n);
 	}
 
-	else if (n<0)
+	else if (n < 0)
 	{
-		printf("%i is negetive\n", n);
+		printf("%i is negative\n", n);
 	}
 
 	else
 	{
-		printf("%i is postive\n", n);
+		printf("%i is positive\n", n);
 	}
 
 	return (0);
